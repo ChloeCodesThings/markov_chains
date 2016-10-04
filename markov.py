3d52baad516a8318e805d05d9948c2f4156139b5
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-04 13:00:08 -0700

    Created open_and_read_file and make_chains functions

---
+++ markov.py
@@ -0,0 +1,70 @@
+from random import choice
+
+
+def open_and_read_file(file_path):
+    """Takes file path as string; returns text as string.
+
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
+
+    return open(file_path).read()
+
+
+def make_chains(text_string):
+    """Takes input text as string; returns _dictionary_ of markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> make_chains("hi there mary hi there juanita")
+        {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
+    """
+
+    text_string = text_string.split()
+
+    chains = {}
+
+    for word_index in range(len(text_string) - 1):
+        try:
+            first_word = text_string[word_index]
+            second_word = text_string[word_index + 1]
+            value = text_string[word_index + 2]
+
+            if (first_word, second_word) in chains:
+                chains[(first_word, second_word)].append(value)
+            else:
+                chains[(first_word, second_word)] = [value]
+
+        except IndexError:
+            pass
+
+    return chains
+
+
+def make_text(chains):
+    """Takes dictionary of markov chains; returns random text."""
+
+    text = ""
+
+    # your code goes here
+
+    return text
+
+
+input_path = "green-eggs.txt"
+
+# Open the file and turn it into one long string
+input_text = open_and_read_file(input_path)
+
+# Get a Markov chain
+chains = make_chains(input_text)
+print chains
+
+# Produce random text
+# random_text = make_text(chains)
+
+# print random_text
